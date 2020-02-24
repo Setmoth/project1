d2a8856bf1890f7e0a2d8fd28630ba3d8352193a
Guy Lagrand <guy@scrmworks.nl>
2020-02-24 12:02:38 +0100

    Update import.py

--- import.py
+++ import.py
@@ -1,24 +1,56 @@
+import os
+import sys
+import io
+
+import SQLAlchemy
+import csv
+
+
+def main():
+    print("Hello World!")
+
 def importCSV():
     """Import data using a csv-file"""
     print(">>>>> importCSV <<<<<")
 
-    if request.method == "POST":
-        # Read CSV file
-        print('>>>>> POST CSV <<<<<')
-        if not request.files["fileX"]:
-            flash('Missing import file, please select a CSV-file from your computer', 'danger')
-            return render_template("import.html") 
-        try:
-            data = pd.read_csv(request.files["fileX"], sep=";")
-            print('>>>>> STORE ROWS IN DB<<<<<')
-            processImport(data)
-            print('>>>>> DONE STORING ROWS IN DB<<<<<')
-            flash('Transactions are stored, you can now process them', 'success')
-        except Exception as e:
-            print(">>>>> exception <<<<<", e)
-            flash('Invalid file or a database error, send an email to slackbyte8@gmail.com', 'danger')
-        return redirect("/")
-    else:
-        print(">>> /GET <<<")
-        flash('Select your csv-file to import the suspense-accounts to be matched', 'info') 
-        return render_template("import.html")   
+    
+    try:
+        data = pd.read_csv(books.csv)
+        print('>>>>> STORE ROWS IN DB<<<<<')
+        processImport(data)
+        print('>>>>> DONE STORING ROWS IN DB<<<<<')
+        flash('Transactions are stored, you can now process them', 'success')
+    except Exception as e:
+        print(">>>>> exception <<<<<", e)
+
+def processImport(data):
+    # for each record store an entry in database (id+key shoul be unique)
+    # isbn,title,author,year
+    
+    # iterate over rows with iterrows()
+    for index, row in data.iterrows():
+         # access data using column names
+         # Skip headers / title
+
+            
+        # Process data
+        csvISBN = row[0]
+        csvTitle = row[1]
+        csvAuthor = row[2]
+        csvYear = row[3]
+        
+
+        #for row in data:
+        params = (csvISBN, csvTitle, csvAuthor, csvYear)
+
+        cursor = db.cursor()
+        cursor.execute('''INSERT INTO books(isbn, title, author, year) VALUES(?, ?, ?, ?)''', params)
+        db.commit()
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)        
+
+
+if __name__ == "__main__":
+    main()    
