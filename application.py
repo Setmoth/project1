e9e18c47cd12da73f75636fdf61a3c1cc35adcd8
Guy Lagrand <guy@scrmworks.nl>
2020-02-25 12:42:04 +0100

    LandingPage done - route to login & register
    add anchor tags so linking actually works

--- application.py
+++ application.py
@@ -39,5 +39,58 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+	print(">>>>> index <<<<<")
 	return render_template("landingPage.html")
     # return "Project 1: TODO"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	print(">>>>> Log user in <<<<<")
+	print("method", request.method)
+	# Forget any user
+	session.clear()
+
+	# User reached route via POST (as by submitting a form via POST)
+	if request.method == "POST":
+
+		# Ensure username was submitted
+		if not request.form.get("username"):
+		    flash("Please, provide your username", 'warning')
+		    return render_template("login.html")
+
+		# Ensure password was submitted
+		elif not request.form.get("password"):
+		    flash("Please, provide your password", 'warning')
+		    return render_template("login.html")
+
+		# Query database for username
+		print(">>>>> Query DB <<<<<") 
+
+		    # Get form information.
+		name = request.form.get("username")
+		try:
+		    username = char(request.form.get("username"))
+		except ValueError:
+		    flash("Your username or password is wrong", 'warning') # For security name both so risk of guessing is lower
+		    return render_template("login.html")
+
+		# Ensure username exists and password is correct
+		for row in rows:
+		    if cursor.fetchone() or not check_password_hash(row[2], request.form.get("password")):
+		        flash("Your username or password is wrong", 'warning') # For security name both so rik of guessing is lower
+		        return render_template("login.html")
+
+		    # Remember which user has logged in
+		    session["user_id"] = row[0]
+
+		    print(">>>>> Redirect user to home page <<<<<")
+		    # Redirect user to home page
+		    flashMessage = "Welcome back, " + request.form.get("username")
+		    flash(flashMessage, 'info')
+		    return redirect("/")
+
+	# User reached route via GET (as by clicking a link or via redirect)
+	else:
+		return render_template("login.html")
+
