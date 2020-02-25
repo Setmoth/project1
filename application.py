dc49a08dde09fdbb4d49b07a91240a83d1bd5a3a
Guy Lagrand <guy@scrmworks.nl>
2020-02-25 21:55:15 +0100

    Registration basics works
    need to add storage in DB

--- application.py
+++ application.py
@@ -67,30 +67,103 @@ def login():
 		# Query database for username
 		print(">>>>> Query DB <<<<<") 
 
-		    # Get form information.
-		name = request.form.get("username")
+		# Get form information.
+		username = request.form.get("username")
 		try:
-		    username = char(request.form.get("username"))
+		    if username == db.execute("SELECT username FROM users WHERE username = :username", 
+		    							{"username": username}).fetchone():
+		    	print("username ", username)
+		    else:
+		    	flash("Your username or password is wrong", 'warning') # For security name both so risk of guessing is lower
+		    	return render_template("login.html")
 		except ValueError:
 		    flash("Your username or password is wrong", 'warning') # For security name both so risk of guessing is lower
 		    return render_template("login.html")
 
 		# Ensure username exists and password is correct
-		for row in rows:
-		    if cursor.fetchone() or not check_password_hash(row[2], request.form.get("password")):
-		        flash("Your username or password is wrong", 'warning') # For security name both so rik of guessing is lower
-		        return render_template("login.html")
+		#for row in rows:
+		if not check_password_hash(row[2], request.form.get("password")):
+			flash("Your username or password is wrong", 'warning') # For security name both so rik of guessing is lower
+			return render_template("login.html")
+		
+		# Remember which user has logged in
+	    #session["user_id"] = row[0]
 
-		    # Remember which user has logged in
-		    session["user_id"] = row[0]
+		print(">>>>> Redirect user to home page <<<<<")
+		# Redirect user to home page
+		flashMessage = "Welcome in the House of Books again, " + request.form.get("username")
+		flash(flashMessage, 'info')
 
-		    print(">>>>> Redirect user to home page <<<<<")
-		    # Redirect user to home page
-		    flashMessage = "Welcome back, " + request.form.get("username")
-		    flash(flashMessage, 'info')
-		    return redirect("/")
+		return redirect("/search")
 
-	# User reached route via GET (as by clicking a link or via redirect)
+	    # User reached route via GET (as by clicking a link or via redirect)
 	else:
 		return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+	print(">>>>> Register User <<<<<")
+
+	if request.method == "POST":
+		
+		if (validateUsername() and  validatePassword()) == True:
+			print("# Do nothing")
+		else:
+			return redirect("/register")
+
+		return render_template("landingPage.html")
+	else:
+		return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+	"""Log user out"""
+
+	# Forget any user_id
+	session.clear()
+
+	# Redirect user to login form
+	return redirect("/")
+
+
+def validateUsername():
+	# Check username
+	if not request.form.get("username"):
+		flash('Please provide a username', 'info')
+		return False
+	else:
+		# Check if username exists in database
+		username = request.form.get("username")
+		
+		try:
+			if username == db.execute("SELECT username FROM users WHERE username = :username", 
+										{"username": username}).fetchone():
+				flash('The username already exists', 'info')
+				return False
+		except:
+			flash('An error occured, please retry', 'error')
+			return False
+	
+	return True
+
+
+def validatePassword():
+	print("Check if a password has been provided")
+	if not request.form.get("password"):
+		flash('Please, provide a password', 'info')
+		return False 
+
+	# Check if password not less than 6 characters
+	if len(request.form.get("password")) < 6:
+	    flash('Password should be a least 6 characters', 'info')
+	    return False 
+
+	print("Check is both password match (password & confirmation)")
+	if request.form.get("password") != request.form.get("confirmation"):
+	    flash('Password don\'t match', 'info')
+	    return False 
+
+	return True   
 
